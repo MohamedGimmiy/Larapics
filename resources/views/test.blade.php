a9a7cdbe6c61d5dc2e3101cbe41fa498221e54ff
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-09-14 20:04:28 +0200

    validating component attributes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
     @php
@@ -12,5 +14,6 @@
     @endphp
     <x-icon :src="$icon" />
     <x-ui.button />
+    <x-alert type="danger"/>
 </body>
 </html>

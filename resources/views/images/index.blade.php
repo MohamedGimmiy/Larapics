a14e605549248763e564b7984d04384fe4cb0659
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-09-15 17:39:36 +0200

    integrating boostrap 5 theme

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,25 +1,30 @@
 <x-layout title="Discover free images">
-    <h1>All images</h1>
-
-    <a href="{{route('images.create')}}">Upload image</a>
-
-    @if ($message = session('message'))
-        <div>{{$message}}</div>
+    <div class="container-fluid mt-4">
+        @if ($message = session('message'))
+        <x-alert type="success" dismissable>
+            {{$component->icon()}}
+            {{$message}}
+        </x-alert>
     @endif
-    @foreach ($images as $image)
-        <div>
-            <div>
-                <a href="{{$image->permalink()}}">
-                    <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" width="300" style="margin-top: 20px;">
-                </a>
+        <div class="row" data-masonry='{"percentPosition": true }'>
+            @foreach ($images as $image)
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="card">
+                    <a href="{{$image->permalink()}}">
+                        <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" class="card-img-top">
+                    </a>
+                    <div class="photo-buttons">
+                        <div>
+                            <a href="{{$image->route('edit')}}" class="btn btn-sm btn-info me-2">Edit</a>
+                            <x-form  action="{{$image->route('destroy')}}" method="DELETE">
+                                <button class="btn btn-sm btn-danger"  type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+                            </x-form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <a href="{{$image->route('edit')}}">Edit</a>
-                <x-form action="{{$image->route('destroy')}}" method="DELETE" style="display: inline;">
-                    <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
-                </x-form>
-            </div>
+            @endforeach
         </div>
-
-    @endforeach
+        {{$images->links()}}
+    </div>
 </x-layout>

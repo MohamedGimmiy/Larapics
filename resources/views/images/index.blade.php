c55f44e8c01b4d602fceb3ea465e1f87bb004cab
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-09-15 16:42:32 +0200

    build app layout using component

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,23 +1,25 @@
-<h1>All images</h1>
+<x-layout title="Discover free images">
+    <h1>All images</h1>
 
-<a href="{{route('images.create')}}">Upload image</a>
+    <a href="{{route('images.create')}}">Upload image</a>
 
-@if ($message = session('message'))
-    <div>{{$message}}</div>
-@endif
-@foreach ($images as $image)
-    <div>
+    @if ($message = session('message'))
+        <div>{{$message}}</div>
+    @endif
+    @foreach ($images as $image)
         <div>
-            <a href="{{$image->permalink()}}">
-                <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" width="300" style="margin-top: 20px;">
-            </a>
+            <div>
+                <a href="{{$image->permalink()}}">
+                    <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" width="300" style="margin-top: 20px;">
+                </a>
+            </div>
+            <div>
+                <a href="{{$image->route('edit')}}">Edit</a>
+                <x-form action="{{$image->route('destroy')}}" method="DELETE" style="display: inline;">
+                    <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+                </x-form>
+            </div>
         </div>
-        <div>
-            <a href="{{$image->route('edit')}}">Edit</a>
-            <x-form action="{{$image->route('destroy')}}" method="DELETE" style="display: inline;">
-                <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
-            </x-form>
-        </div>
-    </div>
 
-@endforeach
+    @endforeach
+</x-layout>

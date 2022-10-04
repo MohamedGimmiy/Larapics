8b77652b5a70fa25d8586eb3fd83b02d8e05aa68
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-10-04 17:22:11 +0200

    creating social factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Image;
+use App\Models\Social;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 
@@ -18,12 +20,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $images = Storage::allFiles('images');
+
         foreach($images as $image){
+            if(strpos($image, ".DS_Stroe")) continue;
             Image::factory()->create([
                 'file' => $image,
                 'dimension' => Image::getDimension($image)
             ]);
         }
+        
+        User::find([2, 4, 6])->each( function ($user) {
+            $user->social()->save(Social::factory()->make());
+        });
     }
 
 

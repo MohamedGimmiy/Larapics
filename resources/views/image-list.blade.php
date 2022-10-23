90c6012f5ccaa1e218e3a550c5ef3caa79015000
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-10-23 15:23:59 +0200

    creating author social links and images

--- resources/views/image-list.blade.php
+++ resources/views/image-list.blade.php
@@ -1,24 +1,6 @@
 <x-layout title="Discover free images">
     <div class="container-fluid mt-4">
-        @if ($message = session('message'))
-        <x-alert type="success" dismissable>
-            {{$component->icon()}}
-            {{$message}}
-        </x-alert>
-    @endif
-        <div class="row" data-masonry='{"percentPosition": true }'>
-            @foreach ($images as $image)
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card">
-                    <a href="{{$image->permalink()}}">
-                        <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" class="card-img-top">
-                    </a>
-
-                </div>
-            </div>
-            @endforeach
-        </div>
-
+        @include('shared._grid-images', ['images' => $images])
     </div>
     {{$images->links()}}
 </x-layout>
